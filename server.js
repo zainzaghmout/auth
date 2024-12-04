6264fec25814d045cf6b2fba7a488cd5a2270f6c
zainzaghmout <zainzaghmout@gmail.com>
2024-12-04 16:24:45 +0300

    adding userRoutes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user'); // Import user routes
 
 // Load environment variables
 require('dotenv').config();
@@ -17,8 +18,9 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
-// Routes
+// Routess
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes); // Register user routes
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
